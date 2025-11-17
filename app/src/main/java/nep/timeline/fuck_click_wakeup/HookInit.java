378a6464f4add9b108be96cbad86f8e7a4ff680b
Timeline <53483352+Nep-Timeline@users.noreply.github.com>
2025-11-18 04:17:25 +0800

    Update HookInit.java

--- app/src/main/java/nep/timeline/fuck_click_wakeup/HookInit.java
+++ app/src/main/java/nep/timeline/fuck_click_wakeup/HookInit.java
@@ -3,6 +3,7 @@ package nep.timeline.fuck_click_wakeup;
 import android.util.Log;
 import android.content.Context;
 import android.view.MotionEvent;
+import android.annotation.SuppressLint;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodReplacement;
@@ -12,6 +13,9 @@ import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class HookInit implements IXposedHookLoadPackage {
+    @SuppressLint("StaticFieldLeak")
+    public static Context context;
+    
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam packageParam) {
         if ("com.android.systemui".equals(packageParam.packageName)) {
@@ -21,15 +25,21 @@ public class HookInit implements IXposedHookLoadPackage {
                 XposedHelpers.findAndHookMethod("com.oplus.systemui.aod.scene.PanoramicAodSingleClickWakeUpController", classLoader, "registerPanoramicAodWakeUpMonitor", XC_MethodReplacement.DO_NOTHING);
                 
                 XposedHelpers.findAndHookMethod("com.oplus.systemui.keyguard.gesture.OplusDoubleClickSleep$OnDoubleClickListener", classLoader, "onSingleTapConfirmed", MotionEvent.class, XC_MethodReplacement.returnConstant(false));
-           
+
+                XposedHelpers.findAndHookConstructor("OplusWakeUpController$AodSingleClickWakeUpCallback", classLoader, "android.content.Context", new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) {
+                        context = (Context) param.args[0];
+                    }
+                });
+                
                 XposedHelpers.findAndHookMethod("com.oplus.systemui.notification.interruption.wakeup.WakeupScreenHelper", classLoader, "powerOnScreen", new XC_MethodHook() {
                     @Override
                     protected void beforeHookedMethod(MethodHookParam param) {
                         param.setResult(null);
-                        Context context = (Context) XposedHelpers.getObjectField(param.thisObject, "mContext");
                         Class<?> companion = XposedHelpers.findClass("com.oplus.systemui.aod.display.OplusWakeUpController$Companion", classLoader);
                         Object controller = XposedHelpers.callStaticMethod(companion, "getInstance", context);
-                        boolean isUpsideDown = XposedHelpers.getBooleanField(companion, "isUpsideDown");
+                        boolean isUpsideDown = XposedHelpers.getBooleanField(controller, "isUpsideDown");
                         if (!isUpsideDown)
                             XposedHelpers.callMethod(controller, "notifyWakeUpCallback", 0);
                     }
