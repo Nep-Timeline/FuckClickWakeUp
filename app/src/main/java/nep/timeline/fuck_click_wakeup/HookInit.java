0752049a547ae7eb44e37187fee2ab1c6ad1c141
Timeline <53483352+Nep-Timeline@users.noreply.github.com>
2025-11-18 05:14:57 +0800

    Comment out AOD show check in HookInit.java
    Commented out the check for 'mAodIsInShow' in HookInit.java.

--- app/src/main/java/nep/timeline/fuck_click_wakeup/HookInit.java
+++ app/src/main/java/nep/timeline/fuck_click_wakeup/HookInit.java
@@ -38,8 +38,8 @@ public class HookInit implements IXposedHookLoadPackage {
                         if (XposedHelpers.getBooleanField(smoothTransitionController, "userEnablePanoramicAllDay"))
                             return;
                         param.setResult(null);
-                        if (XposedHelpers.getBooleanField(aodData, "mAodIsInShow"))
-                            return;
+                        // if (XposedHelpers.getBooleanField(aodData, "mAodIsInShow"))
+                        //     return;
                         Object controller = XposedHelpers.getStaticObjectField(XposedHelpers.findClass("com.oplus.systemui.aod.display.OplusWakeUpController", classLoader), "instance");
                         if (controller == null)
                             return;
